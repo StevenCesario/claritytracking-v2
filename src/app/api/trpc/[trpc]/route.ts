6b7ad3f48ebbfdae0a607b3259874430aa43f557
StevenCesario <steven.lennartsson@gmail.com>
2025-12-13 11:24:18 +0100

    fix(backend): fixed syntax errors in env.js and route.ts

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -12,7 +12,7 @@ const handler = (req: NextRequest) =>
         router: appRouter,
         createContext: () => createTRPCContext({ headers: req.headers }),
         onError:
-            env.env.NODE_ENV === "development"
+            env.NODE_ENV === "development"
                 ? ({ path, error }) => {
                     console.error(`tRPC failed on ${path ?? "<no-path>"}: ${error.message}`);
                 }
--- src/env.js
+++ src/env.js
@@ -9,7 +9,7 @@ export const env = createEnv({
    */
     server: {
         DATABASE_URL: z.url(),
-        CLERK_SECRET_KEY: z.string.min(1),
+        CLERK_SECRET_KEY: z.string().min(1),
         STRIPE_SECRET_KEY: z.string().min(1),
         // Any other secret key is added here
         NODE_ENV: z
